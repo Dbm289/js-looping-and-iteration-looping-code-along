a467d2479cc32b9682c23a184a83cb591460be6e
Dan <DanielMooreRealPerson@gmail.com>
2021-05-25 19:31:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(names, type) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]} for the wonderful ${type[i]} gift!`);
+    }
+    return names;
+}
+
