191eafb6c05e56761b254b148e6a281016c4c3cf
Dan <DanielMooreRealPerson@gmail.com>
2021-05-26 02:13:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // Code your solutions in this file
 function writeCards(names, type) {
+    const allCards = []
+    
     for (let i = 0; i < names.length; i++) {
-        console.log(`Thank you, ${names[i]} for the wonderful ${type[i]} gift!`);
+        allCards.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
     }
-    return names;
+    return allCards;
 }
 
+function countDown(n) {
+
+       while (n >= 0) {
+        console.log(n); 
+        
+        n--;
+       }
+      
+      return n;
+
+    }
